e48b5f5a961a3b9b6bf8d1f852b11d54a36efaab
Mantecorso <luisjuradoquesada@gmail.com>
2018-06-15 11:50:21 +0200

    todavia no funcion, puede que el problema sea el section??

--- public/main.js
+++ public/main.js
@@ -4,6 +4,13 @@ if("geolocation" in navigator) {
         //console.log(position);
         //console.log("Localizacion" + position.coords.latitude, position.coords.longitude);
         //geolocalizacion en bonito
+        var element = document.createElement('img');
+    element.setAttribute('src', "http://maps.googleapis.com/maps/api/staticmap?markers=icon:%7C" + position.coords.latitude + ","+ position.coords.longitude + "&zoom=13&size=400x400&sensor=false");
+    //crea una imagen
+    var doce = document.getElementById("#imagen");
+    //identifica el div con id = imagen
+    document.body.insertBefore(element, doce);
+    //pone la imagen en el div
     }, function (err) {
         console.log(err);
     },{
@@ -14,22 +21,4 @@ if("geolocation" in navigator) {
     } else {
         alert('La geolocalizacion no existe en su ordenador');
     }
-
-var watchID = navigator.geolocation.watchPosition(function(position) {
-    console.log(position)
-
-    var element = document.createElement('img');
-    element.setAttribute('src', "google.es/img.png");
-    //crea una imagen
-}, function (err) {
-    console.log(err);
-},{
-    maximunAge: Infinity, //milisegundos tambien se puede poner tiempo infinito desde la ultima vez que tiene en cache
-    timeout: 10000, //milisegundos tiempo de espera para encontrar la geolocalizacion
-    enableHighAccuracy: true // alta precision
-});
-
-// https://geekshubs.slack.com/archives/G9W9WC0AH/p1529051606000539
-    
-// http://maps.googleapis.com/maps/api/staticmap?markers=icon:%7C{LATITUD},{LONGITUD}&zoom=13&size=400x400&sensor=false
 
